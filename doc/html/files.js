d722def931fccce47ff13ed8db242ff6542f0090
Florent Berbie <florent-berbie@hotmail.fr>
2015-01-19 14:26:12 +0100

    Add About Team section

--- doc/html/files.js
+++ doc/html/files.js
@@ -1,4 +1,4 @@
 var files =
 [
-    [ "FactDev", "dir_a8f1d80779db9cb76079c22e2ff81fcd.html", "dir_a8f1d80779db9cb76079c22e2ff81fcd" ]
+    [ "florent", "dir_01536a2b641ff09914428354c1e7be1e.html", "dir_01536a2b641ff09914428354c1e7be1e" ]
 ];
