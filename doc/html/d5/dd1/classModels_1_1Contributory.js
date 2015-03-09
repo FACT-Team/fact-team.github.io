a602ebd815e6e0bfa58580bb637cb37f2252c495
Travis FACT <ups.fact@gmail.com>
2015-03-09 13:01:03 +0000

    [Travis] Generate Doxygen

--- doc/html/d5/dd1/classModels_1_1Contributory.js
+++ doc/html/d5/dd1/classModels_1_1Contributory.js
@@ -6,6 +6,7 @@ var classModels_1_1Contributory =
     [ "commit", "d5/dd1/classModels_1_1Contributory.html#a2d89036a02bd2828ff76d047338f61ba", null ],
     [ "getDataMap", "d5/dd1/classModels_1_1Contributory.html#a692f563f0428866441ea8bc2b9e772ca", null ],
     [ "getDescription", "d5/dd1/classModels_1_1Contributory.html#ae2b936f2fb1ccad2009eae9b5d12fc02", null ],
+    [ "getLongDescription", "d5/dd1/classModels_1_1Contributory.html#abcd8dce3a913d558b73f17f16d5beb7a", null ],
     [ "getNbHours", "d5/dd1/classModels_1_1Contributory.html#ae85caa255441140fc1a3a0e91531bfc0", null ],
     [ "getProject", "d5/dd1/classModels_1_1Contributory.html#a49379aeb4de2376d5a2aaf10f54daf05", null ],
     [ "hydrat", "d5/dd1/classModels_1_1Contributory.html#a40248b5853045eb46412396513f36b06", null ],
@@ -13,6 +14,7 @@ var classModels_1_1Contributory =
     [ "operator==", "d5/dd1/classModels_1_1Contributory.html#ad49c8b9cdf7254069e07f2238b42c8f3", null ],
     [ "remove", "d5/dd1/classModels_1_1Contributory.html#ab9971d7867516b488095e63e1179eac8", null ],
     [ "setDescription", "d5/dd1/classModels_1_1Contributory.html#a12d4199fa7175c0b43f62eddf7c3d69e", null ],
+    [ "setLongDescription", "d5/dd1/classModels_1_1Contributory.html#a40023cef80233eaa814d7bb41d668322", null ],
     [ "setNbHours", "d5/dd1/classModels_1_1Contributory.html#a1c7d65ee311232d3fd00cf8e4777f1c7", null ],
     [ "setProject", "d5/dd1/classModels_1_1Contributory.html#a4478894daf317068856b707491d03555", null ]
 ];
