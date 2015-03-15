d4ce01c6d892dbc9bc2600e0754fab045279a070
Travis FACT <ups.fact@gmail.com>
2015-03-15 11:23:34 +0000

    [Travis] Generate Doxygen

--- doc/html/d9/dc5/classModels_1_1Rate.js
+++ doc/html/d9/dc5/classModels_1_1Rate.js
@@ -1,8 +1,8 @@
 var classModels_1_1Rate =
 [
+    [ "Rate", "d9/dc5/classModels_1_1Rate.html#a609460ae8c48027ed4ac1aaac416f690", null ],
     [ "Rate", "d9/dc5/classModels_1_1Rate.html#ad2d770be11eb899e8bcf673ed462836e", null ],
     [ "Rate", "d9/dc5/classModels_1_1Rate.html#ab867d2b5b114df71814c742c957badd8", null ],
-    [ "Rate", "d9/dc5/classModels_1_1Rate.html#a609460ae8c48027ed4ac1aaac416f690", null ],
     [ "getDailyRate", "d9/dc5/classModels_1_1Rate.html#a937b0efecbc5b3561ad08fe712165d0b", null ],
     [ "getHourlyRate", "d9/dc5/classModels_1_1Rate.html#ac4c3d9c09bed20d639997da7d6c6798b", null ],
     [ "getLegalRate", "d9/dc5/classModels_1_1Rate.html#aa06f56d236743c66b2e6b1bc772b9233", null ],
--- doc/html/de/d36/classGui_1_1Dialogs_1_1AddProjectDialog.js
+++ doc/html/de/d36/classGui_1_1Dialogs_1_1AddProjectDialog.js
@@ -5,5 +5,6 @@ var classGui_1_1Dialogs_1_1AddProjectDialog =
     [ "accept", "de/d36/classGui_1_1Dialogs_1_1AddProjectDialog.html#abe345ededea4911846a44b984cc04f18", null ],
     [ "checkFields", "de/d36/classGui_1_1Dialogs_1_1AddProjectDialog.html#ab30cf2a880e1746cf629a69fb7d9e7f9", null ],
     [ "fillFields", "de/d36/classGui_1_1Dialogs_1_1AddProjectDialog.html#af31b6ed23acdd5fb8b71caaeddce34f4", null ],
-    [ "reject", "de/d36/classGui_1_1Dialogs_1_1AddProjectDialog.html#a767dcea1ae96d2efc3085f8ade4406ce", null ]
+    [ "reject", "de/d36/classGui_1_1Dialogs_1_1AddProjectDialog.html#a767dcea1ae96d2efc3085f8ade4406ce", null ],
+    [ "setupUI", "de/d36/classGui_1_1Dialogs_1_1AddProjectDialog.html#abfa8eb0a2a03b08fc14c12e425737a62", null ]
 ];
