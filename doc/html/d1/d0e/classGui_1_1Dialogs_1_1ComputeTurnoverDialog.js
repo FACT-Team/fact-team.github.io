4c209908c4e9ab8d74dc0711d3eb5f2450201f86
Travis FACT <ups.fact@gmail.com>
2015-03-16 21:52:40 +0000

    [Travis] Generate Doxygen

--- doc/html/d1/d0e/classGui_1_1Dialogs_1_1ComputeTurnoverDialog.js
+++ doc/html/d1/d0e/classGui_1_1Dialogs_1_1ComputeTurnoverDialog.js
@@ -2,5 +2,7 @@ var classGui_1_1Dialogs_1_1ComputeTurnoverDialog =
 [
     [ "ComputeTurnoverDialog", "d1/d0e/classGui_1_1Dialogs_1_1ComputeTurnoverDialog.html#a0f720f3d04ef26c3086e18a9d1e1606d", null ],
     [ "~ComputeTurnoverDialog", "d1/d0e/classGui_1_1Dialogs_1_1ComputeTurnoverDialog.html#af38c9a19c78923c27b47a00dec44ee16", null ],
-    [ "computeTurnover", "d1/d0e/classGui_1_1Dialogs_1_1ComputeTurnoverDialog.html#ab4d2a48bffed8c09e3d16e2849fd4b0e", null ]
+    [ "beginDateControl", "d1/d0e/classGui_1_1Dialogs_1_1ComputeTurnoverDialog.html#ae7efebd5303635280c02072567298df7", null ],
+    [ "computeTurnover", "d1/d0e/classGui_1_1Dialogs_1_1ComputeTurnoverDialog.html#ab4d2a48bffed8c09e3d16e2849fd4b0e", null ],
+    [ "endDateControl", "d1/d0e/classGui_1_1Dialogs_1_1ComputeTurnoverDialog.html#aec195a2ea796179dbf584dd806077164", null ]
 ];
