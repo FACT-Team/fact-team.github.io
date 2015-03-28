a20f40976c5b6c99b100ccab317007c425d53e7a
Travis FACT <ups.fact@gmail.com>
2015-03-28 15:29:10 +0000

    [Travis] Generate Doxygen

--- doc/html/dir_a02b716cee8d027f23c7b369ea6a1703.js
+++ doc/html/dir_a02b716cee8d027f23c7b369ea6a1703.js
@@ -7,6 +7,5 @@ var dir_a02b716cee8d027f23c7b369ea6a1703 =
     [ "projectmodeltest.h", "d1/d63/projectmodeltest_8h_source.html", null ],
     [ "ratemodeltest.h", "d3/d44/ratemodeltest_8h_source.html", null ],
     [ "searchtest.h", "d4/df4/searchtest_8h_source.html", null ],
-    [ "statisticmodeltest.h", "d3/d8f/statisticmodeltest_8h_source.html", null ],
     [ "usermodeltest.h", "da/d15/usermodeltest_8h_source.html", null ]
 ];
