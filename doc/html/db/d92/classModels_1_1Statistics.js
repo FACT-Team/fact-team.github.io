485ec46082a8c74ed034218acc1c73821848bc11
Travis FACT <ups.fact@gmail.com>
2015-03-28 23:20:42 +0000

    [Travis] Generate Doxygen

--- doc/html/db/d92/classModels_1_1Statistics.js
+++ doc/html/db/d92/classModels_1_1Statistics.js
@@ -1,6 +1,5 @@
 var classModels_1_1Statistics =
 [
     [ "Statistics", "db/d92/classModels_1_1Statistics.html#a60ddd90a571ed4c3ce8c0f6317a36d63", null ],
-    [ "getGlobalTurnover", "db/d92/classModels_1_1Statistics.html#a5ae9c6694d97e6fd4711ca0ce1fcdb2f", null ],
     [ "getTurnoverBetweenDates", "db/d92/classModels_1_1Statistics.html#a2004bf1a547fa92d0f88216897a10a15", null ]
 ];
